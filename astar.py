af5afa9ea7303eb8366c116c49ea43f67882ba8e
Vincent <vincent.mallet96@gmail.com>
2018-01-28 17:12:59 +0100

    Djikstra implemented

--- astar.py
+++ astar.py
@@ -20,7 +20,40 @@ def find_path(Coords, M, start, goal):
         -------
             the shortest path as a list, e.g., [3, 8, 2, 0] where start = 3, goal = 0
     '''
-    # TODO
+    
+    n = shape(M)[0]
+    precedent = [start for i in range(n)]
+    distance_start = [2**14 for i in range(n)]
+    distance_start[start]=0;
+    q = []
+    queue.heappush(q,(start,0))
+    
+    #tant que la queue n'est pas vide, il reste des sommets connectés à start qui n'ont pas été explorés
+    while (q):
+        sommet, distance_sommet = queue.heappop(q)
+        
+        #si le sommet est le but, on retrace le chemin
+        if(sommet==goal):
+            output = [goal]
+            previous = goal
+            while (previous!=start):
+                previous = precedent[previous]
+                output = [previous] + output
+            return output
+        
+        for voisin in range(n):
+            #on selectionne les voisins du sommet et on regarde si il vaut mieux passer par le sommet
+            if (M[sommet,voisin]):
+                dist_par_sommet = distance_start[sommet]+M[sommet, voisin]
+                #si oui, on note la nouvelle distance et le précédent, et on met le voisin dans la queue
+                #Attention, le voisin peut rentrer plusieurs fois dans la queue mais l'inégalité est stricte 
+                #donc ses voisins ne peuvent rentrer qu'une fois dans la queue par ce sommet
+                if(dist_par_sommet < distance_start[voisin]): 
+                    distance_start[voisin] = dist_par_sommet
+                    precedent[voisin] = sommet
+                    queue.heappush(q,(voisin, dist_par_sommet))
+                    
+    #Si on a pas atteint le noeud goal et que la liste est vide, les noeuds ne sont pas reliés         
     return [-1]
 
 
--- run_task1.py
+++ run_task1.py
@@ -3,12 +3,13 @@ from astar import find_path
 
 # Edge cost matrix
 M = loadtxt('M.dat')
+
 N,N = M.shape
 # Node positions 
 XX = loadtxt('Nodes.dat')
 
 # Find the shortest path using A*
-path = find_path(XX,M,0,N-1)
+path = find_path(XX,M,0,12)
 
 # Print the path
 print("Path: %s" % str(path))
